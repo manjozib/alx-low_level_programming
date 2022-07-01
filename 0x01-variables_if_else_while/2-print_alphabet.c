4db9f25acb004ca2b808094efc4e93fe3c2901e4
systemsAnalysts <manjozib@gmail.com>
2022-07-01 14:20:57 -0700

    commiting task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+for (char letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
