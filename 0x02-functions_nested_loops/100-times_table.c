3d6c976b01b70ddca807c36a684affe46870dc43
systemsAnalysts <manjozib@gmail.com>
2022-07-06 11:12:11 -0700

    recommiting task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_times_table - prints the n times table, starting with 0
 * @n: number of the times table
@@ -30,8 +29,7 @@ _putchar(k + '0');
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
+_putchar(' ');_putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
