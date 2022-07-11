7133db59405bc34fe143c3e1816490b30e411054
systemsAnalysts <manjozib@gmail.com>
2022-07-11 05:36:52 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strlen - it returns length of string iputed
+*@s: string to be calculated its length
+*@p: pointer to the string s
+*@i: length of string
+*
+* Return : nothing
+*/
+
+int _strlen(char *s)
+{
+char *p = s;
+int i = 0;
+
+while (*p != '\0')
+{
+i++;
+p++;
+}
+
+return (i);
+
+}
