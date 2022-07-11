8f2bf4e22165d3b35d09d55c0df3e6b875f31d4c
systemsAnalysts <manjozib@gmail.com>
2022-07-11 05:04:33 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*swap - swap 2 variables a and b
+*@a a pointer which will take value of b
+*@b a pointer which will take value of a
+*Returns : nothin
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a =  *b;
+*b = temp;
+}
